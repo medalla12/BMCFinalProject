c3755ec327cd133621bbc9b5ae663d37461da727
medalla12 <168712302+medalla12@users.noreply.github.com>
2025-10-30 09:59:44 +0000

    Flutter 3.32.5 V Firebase Integration Part 1

---
+++ android/app/src/main/kotlin/com/example/andrei_303/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.andrei_303
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:andrei_303/screens/login_screen.dart';
+import 'package:andrei_303/screens/signup_screen.dart';
+
+void main() async {
+
+  // 1. Preserve the splash screen
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // 2. Initialize Firebase (from Module 1)
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 3. Run the app (from Module 1)
+  runApp(const MyApp());
+
+  // 4. Remove the splash screen after app is ready
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 1. MaterialApp is the root of your app
+    return MaterialApp(
+      // 2. This removes the "Debug" banner
+      debugShowCheckedModeBanner: false,
+      title: 'eCommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      // 3. A simple placeholder for our home screen
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My E-Commerce App'),
+        ),
+        body: Center(
+          child: Text('Firebase is Connected!'),
+        ),
+      ),
+    );
+  }
+}
